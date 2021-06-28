495ca2b4a023108b3e05e31fbce77dd79c58e21d
Cloud User <centos@ip-172-31-19-91.ca-central-1.compute.internal>
2021-06-28 00:31:04 +0000

    establish connection to database

---
+++ app/schema.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS post;
+
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE post (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    author_id INTEGER NOT NULL,
+    date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL,
+    like_count INTEGER NOT NULL DEFAULT 0,
+    img TEXT NOT NULL,
+    img_mimetype TEXT NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES user (id)
+);
