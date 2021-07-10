d1fb8e15748e1e51b112cf7b6a74339f0314ee0b
Anh Pham <thuanhsone99@gmail.com>
2021-07-09 19:34:00 -0700

    Revert "Add auto_fix to lint test"

--- models.py
+++ models.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from logging import NullHandler
 from db import db
 
-
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
